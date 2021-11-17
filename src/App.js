94e93b7ae22416da1335439239191a79b9eb73bc
Kriistoffer <kjohansson93@hotmail.se>
2021-11-17 11:59:54 +0100

    Preparing the frontend routing of pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navigationbar from './components/navbar/Navigationbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navigationbar/>
+      <Router>
+        <Navigationbar />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/GamePage' component={GamePage} />
+          <Route path='/ConsolePage' component={ConsolePage} />
+          <Route path='/ControllerPage' component={ControllerPage} />
+          <Route path='/AdminPage' component={AdminPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
