964d9d99ea14e1806a7a0778b5a66186be79e133
Vivi Tran <vihanhtranjobb@hotmail.com>
2021-11-17 13:34:28 +0100

    Remove dropdown menu in navbar, added links instead

--- src/components/navbar/Navigationbar.js
+++ src/components/navbar/Navigationbar.js
@@ -13,14 +13,9 @@ const Navigationbar = () => {
     <Container className="navbar-container">
     <Navbar.Brand href="#home"><img className="game-start-logo"src={GameStartLogo}/></Navbar.Brand>
     <Nav className="me-auto">
-    <NavDropdown title="Products" id="navbarScrollingDropdown" className="navbarDropDown">
-          <NavDropdown.Item href="#games">Games</NavDropdown.Item>
-          <NavDropdown.Divider/>
-          <NavDropdown.Item href="#consoles">Consoles</NavDropdown.Item>
-          <NavDropdown.Divider/>
-          <NavDropdown.Item href="#controllers">Controllers</NavDropdown.Item>
-        </NavDropdown>
-        
+    <Nav.Link className="category-link" href="#games">GAMES</Nav.Link>
+    <Nav.Link className="category-link"href="#consoles">CONSOLES</Nav.Link>
+    <Nav.Link className="category-link"href="#controllers">CONTROLLERS</Nav.Link>  
     </Nav>
     </Container>
     <BsCart4 className="cart-icon" size="2rem"/>
