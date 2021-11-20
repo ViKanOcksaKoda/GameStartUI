a9b43ee4f16f7295b05e41be3a162b01b9111a2c
Kriistoffer <kjohansson93@hotmail.se>
2021-11-20 23:29:06 +0100

    The beginning of the shoppingcart logic

--- src/components/shoppingcart/ShoppingCart.js
+++ src/components/shoppingcart/ShoppingCart.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import service from "../../service";
 
-const ShoppingCart = () => {
+const ShoppingCart = (CustomerID) => {
 
-    
+    const [GetCurrentShoppingcart, setCurrentShoppingcart] = useState([]);
+    const [CustomerID, setCustomerID] = useState();
+
+    setCustomerID(CustomerID);
+    setCurrentShoppingcart(Service.GetShoppingcart(CustomerID));
+
+    const RemoveItemFromCart = (ProductID) => {
+        Service.RemoveItemFromCart(ProductID);
+    };
+
+    const EmptyCart = (CustomerID) => {
+        Service.EmptyCart(CustomerID);
+    };
+
+    const IncreaseQuantity = () => {
+        //Genomför databaskoll ifall det finns mer utav produkten innan det godkänns
+    };
+
+    const DecreaseQuantity = () => {
+        //Genomför koll ifall quantity når 0
+    };
+
+    const ProcessPurchase = () => {
+        //Genomför köp
+    };
+
     return (
         <div>
-
+            
         </div>
     );
 };
