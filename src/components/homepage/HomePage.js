5a3c2d2b34a7add6b541f64257ef2b8d8b665f60
Kriistoffer <kjohansson93@hotmail.se>
2021-11-21 22:38:27 +0100

    WORK IN PROGRESS

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -1,7 +1,37 @@
 import CarouselSlider from "../carousel/CarouselSlider";
+import Service from "../../service";
+import { useEffect } from "react";
+// import axios from "axios";
 
 const HomePage = () => {
-    return(
+
+    // const baseURL = "https://localhost:7024/api/products/1";
+
+    // useEffect(() => {
+    //     axios.get(baseURL).then((response) => {
+    //         console.log(response);
+    //     });
+    // }, []);
+
+    //KODEN OVAN FUNKAR, MEN RESULTATET BEHÖVS KOLLAS ÖVER -- TRIM MUCH FAT YES.
+
+    // useEffect(() => {
+    //     const response = fetch(baseURL)
+    //     .then(response => response.json())
+    //     .then(data => console.log(data));
+
+
+    //     });
+
+    //KOD OVAN FUNKAR BÄTTRE MEN JAG GILLAR DEN MINDRE. RESULTATET VERY NICE THOUGH.
+
+    // const GetProduct = () => {
+    //     Service.GetAllCategories();
+    // };
+
+    // console.log(Service.GetProductById(2));
+
+    return( 
         <CarouselSlider/>
     );
 }
