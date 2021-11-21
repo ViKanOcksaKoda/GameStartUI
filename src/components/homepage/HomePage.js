e926c6cbd5e73057fb389908a544ed8014897241
Kriistoffer <kjohansson93@hotmail.se>
2021-11-21 23:25:07 +0100

    Work in progress -- api data not quite working as intended

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -1,11 +1,11 @@
 import CarouselSlider from "../carousel/CarouselSlider";
 import Service from "../../service";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // import axios from "axios";
 
 const HomePage = () => {
 
-    // const baseURL = "https://localhost:7024/api/products/1";
+    const baseURL = "https://localhost:7024/api/";
 
     // useEffect(() => {
     //     axios.get(baseURL).then((response) => {
@@ -19,8 +19,6 @@ const HomePage = () => {
     //     const response = fetch(baseURL)
     //     .then(response => response.json())
     //     .then(data => console.log(data));
-
-
     //     });
 
     //KOD OVAN FUNKAR BÄTTRE MEN JAG GILLAR DEN MINDRE. RESULTATET VERY NICE THOUGH.
@@ -31,8 +29,52 @@ const HomePage = () => {
 
     // console.log(Service.GetProductById(2));
 
-    return( 
-        <CarouselSlider/>
+    const [productName, setProductName] = useState();
+    const [Name, setName] = useState();
+    const [Price, setPrice] = useState();
+    const [Id, setId] = useState();
+    const [Description, setDescription] = useState();
+    const [Quantity, setQuantity] = useState();
+
+    const getProduct = (id) => {
+        Service.GetProductById(id)
+            .then((res) => {
+                setProductName(res.data);
+                console.log(productName);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    };
+
+    useEffect(() => {
+        getProduct(2);
+    }, []);
+
+
+    
+        useEffect(() => {
+            fetch(baseURL + "products/2")
+                .then(response => response.json())
+                .then(data => {
+                    setName(data.name);
+                    setPrice(data.price);
+                    setId(data.id);
+                    setDescription(data.description);
+                    setQuantity(data.stockBalance);
+
+                    console.log("Namn: " + Name);
+                    console.log("Price: " + Price);
+                    console.log("ID: " + Id);
+                    console.log("Description: " + Description);
+                    console.log("Quantity: " + Quantity);
+                })
+        }, []);
+
+    return (
+        <>
+            <CarouselSlider />
+        </>
     );
 }
 
--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
   export default axios.create({
     
-    baseURL: "https://localhost:7024",
+    baseURL: 'https://localhost:7024',
     headers: {
         "Content-Type": "application/json"
         // 'ApiKey': process.env.REACT_APP_API_KEY
