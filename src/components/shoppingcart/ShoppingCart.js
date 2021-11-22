0a43aa806019dec0c10bd1aa4c9c2c6249601e52
Kriistoffer <kjohansson93@hotmail.se>
2021-11-22 13:30:03 +0100

    Work in progress

--- src/components/shoppingcart/ShoppingCart.js
+++ src/components/shoppingcart/ShoppingCart.js
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import service from "../../service";
 
-const ShoppingCart = (CustomerID) => {
+const ShoppingCart = (id) => {
 
-    const [GetCurrentShoppingcart, setCurrentShoppingcart] = useState([]);
+    const baseURL = "https://localhost:7024/";
+
+    const [CurrentShoppingcart, setCurrentShoppingcart] = useState([]);
     const [CustomerID, setCustomerID] = useState();
 
     setCustomerID(CustomerID);
-    setCurrentShoppingcart(Service.GetShoppingcart(CustomerID));
+    // setCurrentShoppingcart(Service.GetShoppingcart(CustomerID));
+
+    useEffect(() => {
+        fetch(baseURL + "api/shoppingcart/get")
+            .then(response => response.json())
+            .then(data => {
+                setCurrentShoppingcart(data);
+            })
+    }, [])
 
     const RemoveItemFromCart = (ProductID) => {
-        Service.RemoveItemFromCart(ProductID);
+        // Service.RemoveItemFromCart(ProductID);
     };
 
     const EmptyCart = (CustomerID) => {
-        Service.EmptyCart(CustomerID);
+        // Service.EmptyCart(CustomerID);
     };
 
     const IncreaseQuantity = () => {
@@ -29,8 +39,19 @@ const ShoppingCart = (CustomerID) => {
         //Genomför köp
     };
 
+    // const GetProductName = (id) = {
+    //     fetch(baseURL + `api/products/${id}`)
+    // }
+
     return (
         <div>
+            {CurrentShoppingcart.map(function(d, idx) {
+                return (<li key={idx}>{d.ProductId}{d.Quantity}</li>)
+            })}
+
+
+
+
             {/* Kod för att lägga till rätt antal "productcards". Varje productcard innehåller namn på produkt, kanske beskrivning/del av beskrivning, antal utav produkten,
             möjlighet att lägga till/ta bort antalet utav specifik produkt, totalpris, och möjligheten att ta bort en produkt helt och hållet - oavsett antal.
 
@@ -40,3 +61,5 @@ const ShoppingCart = (CustomerID) => {
         </div>
     );
 };
+
+export default ShoppingCart;
