2aea6dc9513898a214150cc000e35e55c5e6ddd0
Vivi Tran <vivitraan3@gmail.com>
2021-11-24 14:39:31 +0100

    Add useState hook for username and password

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,36 +1,50 @@
 import React from "react";
 import { Form, Button, FloatingLabel } from "react-bootstrap";
 import Container from "react-bootstrap/Container"
-import {useState} from "react";
+import { useState, useEffect } from "react";
 import "./LoginPage.css";
 
 
 const LoginPage = () => {
-    const [usernameInput, setUsernameInput] = useState();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  
+  const [show, setShow] = useState(false);
+  con
 
-    const onHandleSubmit = () => {
+  const onHandleSubmit = () => {
 
-    }
-    return (
-        <Container className="login-container">
-            <h1 className="login-label">Login</h1>
-        <Form className="LoginForm">
-            <FloatingLabel
-    controlId="floatingInput"
-    label="Email address"
-    className="mb-3"
-  >
-    <Form.Control type="email" placeholder="name@example.com" />
-  </FloatingLabel>
-  <FloatingLabel controlId="floatingPassword" label="Password">
-    <Form.Control type="password" placeholder="Password" />
-  </FloatingLabel>
-        </Form>
-        <Button className="submit-button mt-4" variant="outline-dark" type="submit">
-                Submit
-            </Button>
-        </Container>
-    );
+  }
+
+  return (
+    <Container className="login-container">
+      <h1 className="login-label">Login</h1>
+      <Form className="LoginForm">
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Username"
+          className="mb-3"
+        >
+          <Form.Control type="string" placeholder="Username" value={username} 
+          onChange={event => setUsername(event.target.value)}
+          />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingPassword" label="Password" value={password} 
+          onChange={event => setPassword(event.target.value)}>
+          <Form.Control type="password" placeholder="Password" />
+        </FloatingLabel>
+      </Form>
+      {error && <p className="error">{error}</p>}
+      <Button className="submit-button mt-4" variant="outline-dark" type="submit">
+        Submit
+      </Button>
+    </Container>
+  );
 };
+
+//onHandleSubmit ska hämta data
+//onClick i button för att hämta onHandleSubmit
 
 export default LoginPage;
