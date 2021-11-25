8594b12dd96eedfa9aa92c8adc2c5a2c168f4b0d
Kriistoffer <kjohansson93@hotmail.se>
2021-11-25 16:49:07 +0100

    Added usecontexts for global variables

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import Navigationbar from './components/navbar/Navigationbar';
 import HomePage from './components/homepage/HomePage';
@@ -6,23 +7,31 @@ import ConsolePage from './components/consolepage/ConsolePage';
 import ControllerPage from './components/controllerpage/ControllerPage';
 import RegistrationPage from './components/registration/RegistrationPage';
 import './App.css';
+import { UserIdContext } from "./components/context/UserIdContext";
+import { LoggedInContext } from "./components/context/LoggedInContext";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userId, setUserId] = useState(null);
+
   return (
     <Router>
       <div className="App">
-        <Navigationbar />
-        <Routes>
-          <Route path='/' element={ <HomePage /> } />
-          <Route path='/GamePage' element={ <GamePage />} />
-          <Route path='/ConsolePage' element={ <ConsolePage />} />
-          <Route path='/ControllersPage' element={ <ControllerPage />} />
-          {/* <Route path='/AdminPage' element={ <AdminPage /> } /> */}
-          <Route path='/SignUp' element={ <RegistrationPage /> } /> 
-        </Routes>
+        <UserIdContext.Provider value={{ userId, setUserId }}>
+          <LoggedInContext.Provider value={{ loggedIn, setLoggedIn }}>
+            <Navigationbar />
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/GamePage' element={<GamePage />} />
+              <Route path='/ConsolePage' element={<ConsolePage />} />
+              <Route path='/ControllersPage' element={<ControllerPage />} />
+              {/* <Route path='/AdminPage' element={ <AdminPage /> } /> */}
+              <Route path='/SignUp' element={<RegistrationPage />} />
+            </Routes>
+          </LoggedInContext.Provider>
+        </UserIdContext.Provider>
       </div>
-    </Router>
-
+    </Router >
   );
 }
 
