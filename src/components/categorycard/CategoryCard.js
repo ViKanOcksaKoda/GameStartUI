89ef9314b42d296ed4d6ea538a929855d5ff310a
Vivi Tran <vihanhtranjobb@hotmail.com>
2021-11-27 11:43:52 +0100

    Fix so cards are besida eachother

--- src/components/categorycard/CategoryCard.js
+++ src/components/categorycard/CategoryCard.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
 import "./CategoryCard.css";
 
 export default class CategoryCard extends Component {
-    static displayName = CategoryCard.name;
+  static displayName = CategoryCard.name;
 
-    render() {
-        return(
-            <div className="card">
-            <img className="card-img-top .info_card_image imageSize" src={this.props.infoImage}/>
-            <div className="card-body">
-            <p className="cardTitleText">
-                {this.props.cardTitleText}
-              </p>
-              <p className="card-text">
-                {this.props.infoText}
-              </p>
-              <p className="cardPriceText">
-                {this.props.priceText}
-              </p>
-              <a href="#" class="btn cart px-auto">ADD TO CART</a>
-            </div>
+  render() {
+    return (
+      <Container className="card-container">
+        <div className="card">
+          <img
+            className="card-img-top .info_card_image imageSize"
+            src={this.props.infoImage}
+          />
+          <div className="card-body">
+            <p className="cardTitleText">{this.props.cardTitleText}</p>
+            <p className="card-text">{this.props.infoText}</p>
+            <p className="cardPriceText">{this.props.priceText}</p>
+            <a href="#" class="btn cart px-auto">
+              ADD TO CART
+            </a>
           </div>
-        );
-    }
-
+        </div>
+      </Container>
+    );
+  }
 }
