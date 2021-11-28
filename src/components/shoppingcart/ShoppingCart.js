728f959bb9f2a21097ba4e5052c8cc4dc59c812d
Vivi Tran <vihanhtranjobb@hotmail.com>
2021-11-28 15:31:45 +0100

    Add table to shopping cart!

--- src/components/shoppingcart/ShoppingCart.js
+++ src/components/shoppingcart/ShoppingCart.js
@@ -1,49 +1,62 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { UserContext } from '../context/UserContext';
+import React, { useState, useEffect, useContext } from "react";
+import { UserContext } from "../context/UserContext";
 import Container from "react-bootstrap/Container";
-import axios from 'axios';
+import Table from "react-bootstrap/Table";
+import axios from "axios";
+import "./ShoppingCart.css";
 
 const ShoppingCart = () => {
-    const { user, setUser } = useContext(UserContext)
+  const { user, setUser } = useContext(UserContext);
 
-    const getShoppingcart = () => {
-        axios.get(`https://localhost:7024/api/shoppingcart/${user}`)
-            .then((response) => {
-                console.log(response.data.cartItems.shoppingCartItem);
-            })
-            .catch((error) => console.log(`Error: ${error}`));
+  const getShoppingcart = () => {
+    axios
+      .get(`https://localhost:7024/api/shoppingcart/${user}`)
+      .then((response) => {
+        console.log(response.data.cartItems.shoppingCartItem);
+      })
+      .catch((error) => console.log(`Error: ${error}`));
+  };
+
+  useEffect(() => {
+    getShoppingcart();
+  }, []);
+
+  const EmptyCart = () => {
+    axios.get(`https://localhost:7024/api/shoppingcart/`);
+  };
+
+  const ProcessPurchase = () => {
+    //Genomför köp
+  };
+
+  const displayShoppingcart = () => {
+    {
     }
+  };
 
-    useEffect(() => {
-        getShoppingcart();
-    }, [])
-
-    const EmptyCart = () => {
-        axios.get(`https://localhost:7024/api/shoppingcart/`)
-    };
-
-    const ProcessPurchase = () => {
-        //Genomför köp
-    };
-
-    const displayShoppingcart = () => {
-        {
-
-        }
-    }
-
-    return (
-        <Container className="top-container">
-            <Container className="shoppingcart-container">
-                <h1 className="shoppingcartFont"> SHOPPINGCART </h1>
-                <div className="row">
-                    <div className="col category-card card-padding">
-
-                    </div>
-                </div>
-            </Container>
-        </Container>
-    );
+  return (
+    <Container className="shoppingcart-container">
+      <h1 className="shoppingcart-title">Shopping Cart</h1>
+      <Table responsive className="shoppingcart-table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Product ID</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            {Array.from({ length: 3 }).map((_, index) => (
+              <td key={index}>Table cell {index}</td>
+            ))}
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
+  );
 };
 
 export default ShoppingCart;
