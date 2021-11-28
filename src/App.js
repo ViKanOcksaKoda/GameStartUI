0eab291097d39b84b86933d925311ba203344dec
Kriistoffer <kjohansson93@hotmail.se>
2021-11-28 10:17:31 +0100

    Imported contexts and wrapped the components in context providers

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import Navigationbar from './components/navbar/Navigationbar';
 import HomePage from './components/homepage/HomePage';
@@ -7,22 +8,35 @@ import ControllerPage from './components/controllerpage/ControllerPage';
 import AdminPage from './components/adminpage/AdminPage';
 import LoginPage from './components/login/LoginPage';
 import RegistrationPage from './components/registration/RegistrationPage';
+import { StatusContext } from './components/context/StatusContext';
+import { UserContext } from './components/context/UserContext';
+import { RoleContext } from './components/context/RoleContext';
 import './App.css';
 
 function App() {
+  const [status, setStatus] = useState(false);
+  const [user, setUser] = useState(null);
+  const [role, setRole] = useState("guest");
+
   return (
     <Router>
       <div className="App">
-        <Navigationbar />
-        <Routes>
-          <Route path='/' element={ <HomePage /> } />
-          <Route path='/GamePage' element={ <GamePage />} />
-          <Route path='/ConsolePage' element={ <ConsolePage />} />
-          <Route path='/ControllersPage' element={ <ControllerPage />} />
-          <Route path='/LogIn' element={<LoginPage />} />
-          <Route path='/AdminPage' element={ <AdminPage /> } />
-          <Route path='/SignUp' element={ <RegistrationPage /> } /> 
-        </Routes>
+        <StatusContext.Provider value={{ status, setStatus }}>
+          <UserContext.Provider value={{ user, setUser }}>
+            <RoleContext.Provider value={{ role, setRole }}>
+              <Navigationbar />
+              <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/GamePage' element={<GamePage />} />
+                <Route path='/ConsolePage' element={<ConsolePage />} />
+                <Route path='/ControllersPage' element={<ControllerPage />} />
+                <Route path='/LogIn' element={<LoginPage />} />
+                <Route path='/AdminPage' element={<AdminPage />} />
+                <Route path='/SignUp' element={<RegistrationPage />} />
+              </Routes>
+            </RoleContext.Provider>
+          </UserContext.Provider>
+        </StatusContext.Provider>
       </div>
     </Router>
 
