2094c606c5cb8a5d4fdea1891e31e3686b21ff6a
Kriistoffer <kjohansson93@hotmail.se>
2021-12-08 23:17:45 +0100

    Removed all individual axios calls and replaced them with service calls

--- src/components/controllerpage/ControllerPage.js
+++ src/components/controllerpage/ControllerPage.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
 import CategoryCard from "../categorycard/CategoryCard";
 import Container from "react-bootstrap/Container";
-import axios from "axios";
+import Service from "../../service";
+import "./ControllerPage.css";
 
 const ControllerPage = () => {
   const [data, setData] = useState([]);
-  const getAllControllerData = () => {
-    axios
-      .get(`https://localhost:7024/api/products/categoryproducts?CategoryId=3`)
-      .then((response) => {
-        setData(response.data.products);
+
+  const GetProducts = (id) => {
+    Service.GetProductsByCategory(id)
+      .then((res) => {
+        setData(res.data.products);
       })
-      .catch((error) => console.error(`Error: ${error}`));
+      .catch((error) => console.log(`Error: ${error}`))
   };
+
+  useEffect(() => {
+    GetProducts(3);
+  }, []);
 
   const displayData = data.map((data) => (
     <CategoryCard
@@ -24,15 +29,10 @@ const ControllerPage = () => {
     />
   ));
 
-  useEffect(() => {
-    getAllControllerData();
-  }, []);
-
   return (
     <Container className="top-container">
       <Container className="game-container">
         <h1 className="categoryFont"> CONTROLLERS </h1>
-
         <div className="row">
           <div className="col category-card card-padding">{displayData}</div>
         </div>
@@ -40,4 +40,5 @@ const ControllerPage = () => {
     </Container>
   );
 };
+
 export default ControllerPage;
